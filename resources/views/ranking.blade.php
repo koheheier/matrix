4d564f67ef47d668bf2cb5fbe97c437650dffec5
koheheier <thekouhey-0208-khe@ezweb.ne.jp>
2023-05-29 10:13:52 +0900

    ランキングのトップ3に色付けしたよ

--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -2,28 +2,52 @@
     <div class="text-xl">
         {{ $matrix_name }}
     </div>
-
-    <div class="justify-items-center rank_field" id="">
+    @php
+        $bg_color_classes = 
+        [
+            "bg-yellow-200",
+            "bg-gray-500",
+            "bg-amber-600"
+        ];
+        $bg_color_classes_json = json_encode($bg_color_classes);
+    @endphp
+    <div class="justify-items-center">
         @foreach($candidates_points as $group_id => $candidate_points)
-            <div class="candidate bg-red-200 border candidate_{{ $group_id }} rank_{{ $loop->index }}" id="">
-                <h1>{{ $candidate_names[$group_id] }}</h1>    
+            <div class="candidate border candidate_{{ $group_id }}">
+                <h1 class="bg-black text-3xl text-white">
+                    {{ $candidate_names[$group_id] }}
+                    @if($loop->index < 3)
+                        @if($loop->first)
+                            👑
+                        @endif
+                        @php
+                            $bg_class = $bg_color_classes[$loop->index];
+                        @endphp
+                    @else
+                        @php
+                            $bg_class = "bg-white";
+                        @endphp
+                    @endif
+                </h1>
                 @foreach($candidate_points as $factor_id => $candidate_point)
-                    <div class="factor_{{ $factor_id }} text-black bg-red-200" onClick="activeSwitch({{ $factor_id }})">
+                    <div class="factor_{{ $factor_id }} text-black {{ $bg_class }} text-3xl" onClick="activeSwitch({{ $factor_id }})">
                         <input type="hidden" class="candidate_factor_points_{{ $factor_id }}" value="{{ $factors_data[$factor_id]['weight'] * $candidate_point }}">
                         <input type="hidden" class="group_id_{{ $factor_id }}" value="{{ $group_id }}">
                         <input type="hidden" value="1" class="calcBool_{{ $factor_id }}">
                         {{ $factors_data[$factor_id]["name"] }} : {{ $factors_data[$factor_id]["weight"] }} × ポイント：{{ $candidate_point }}
                     </div>
-                    @endforeach
+                @endforeach
                     <input type="hidden" id="{{ $group_id }}" value="{{ $candidate_totals[$group_id] }}">
-                    合計：<span id="total_{{ $group_id }}">{{ $candidate_totals[$group_id] }}</span>
+                    <div class="bg-red-500 font-bold text-2xl text-white">
+                        合計：<span id="total_{{ $group_id }}">{{ $candidate_totals[$group_id] }}</span>
+                    </div>
                 </div>
             </div>
         @endforeach
     </div>
 
     <script>
-        
+        var bg_color_classes = JSON.parse('<?php echo $bg_color_classes_json; ?>');
 
         /* 要素クリックによって非アクティブ状態にし、再計算 */
         function activeSwitch(factor_id){
@@ -32,18 +56,24 @@
             var candidate_factor_points = document.getElementsByClassName("candidate_factor_points_" + factor_id);
             var group_ids = document.getElementsByClassName("group_id_" + factor_id);
             var totals = [];
+            var bg_color_class = "";
             for (var i = 0; i < calcBools.length; i++) {
+                if (i < 3) {
+                    bg_color_class = bg_color_classes[i];
+                } else {
+                    bg_color_class = "bg-white";
+                }
                 var group_id = group_ids[i].value;
                 if (calcBools[i].value > 0) {
                     calcBools[i].value = 0;
-                    selected_factors[i].classList.replace('bg-red-200', 'bg-gray-200');
+                    selected_factors[i].classList.replace(bg_color_class, 'bg-gray-200');
                     selected_factors[i].classList.replace('text-black', 'text-gray-400');
                     //  合計から引く
                     document.getElementById('total_' + group_id).textContent
                         = parseInt(document.getElementById('total_' + group_id).textContent, 10) - parseInt(candidate_factor_points[i].value);
                 } else {
                     calcBools[i].value = 1;
-                    selected_factors[i].classList.replace('bg-gray-200', 'bg-red-200');
+                    selected_factors[i].classList.replace('bg-gray-200', bg_color_class);
                     selected_factors[i].classList.replace('text-gray-400', 'text-black');
                     //  合計に足す
                     document.getElementById('total_' + group_id).textContent
@@ -60,41 +90,5 @@
         }
     </script>
 
-<div class="rank_field">
-    <div class="rank_1">1</div>
-    <div class="rank_2">2</div>
-    <div class="rank_0">3</div>
-</div>
-
-
-
-
-
-<style>
-    /* 候補ごとのポイントと要素重みを計算し、順番を返す */
-
-    /* 受け取った順番で、候補ボックスを並べ替える */
-
-    .rank_field {
-        display: flex;
-        flex-direction:column;
-    }
-
-    .rank_0 {
-        order: 4;
-    }
-    .rank_1 {
-        order: 1;
-    }
-    .rank_2 {
-        order: 2;
-    }
-    .rank_3 {
-        order: 3;
-    }
-    .rank_4 {
-        order: 0;
-    }
-</style>
 </x-layout.base>
 
